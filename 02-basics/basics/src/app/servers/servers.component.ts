46e48987b1c5ca635c171a8287a72d72fe1ce8db
Maykal Tenev <michaeltenev@gmail.com>
2023-02-01 20:40:14 +0100

    assigment 2

--- 02-basics/basics/src/app/servers/servers.component.ts
+++ 02-basics/basics/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'test';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
