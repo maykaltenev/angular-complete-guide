c5ad71626dd507599e45e4d25dce4e23f62187a0
Maykal Tenev <michaeltenev@gmail.com>
2023-02-02 10:49:29 +0100

    ngFor on list of recepies

--- 03-course-project/recepie-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ 03-course-project/recepie-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
-export class RecipesListComponent {
-
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://lh3.googleusercontent.com/2LfchMbmtxopMRN_TM9QEDtaugIdpOBDF-SZf0xWcGOkQXKJ3fD3-BdRNrodYHyeI2nmPJKxKu6js_RejcB5zmY3CePfwF6Aqz1eMZPC=w1200-l75'
+    ),
+  ];
+  constructor() {}
+  ngOnInit() {}
 }
