b76c41848fb04ecd0e6503879e45fa7410510456
Maykal Tenev <michaeltenev@gmail.com>
2023-02-02 10:08:28 +0100

    recepie app and structure

---
+++ 03-course-project/recepie-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShippingEditComponent } from './shopping-list/shipping-edit/shipping-edit.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShippingEditComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ 03-course-project/recepie-app/src/app/shopping-list/shipping-edit/shipping-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-edit',
+  templateUrl: './shipping-edit.component.html',
+  styleUrls: ['./shipping-edit.component.css']
+})
+export class ShippingEditComponent {
+
+}
