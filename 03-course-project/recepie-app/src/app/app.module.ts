11908f3a9e2b2a1137c5bbbc75154644ae28851c
Maykal Tenev <michaeltenev@gmail.com>
2023-02-02 10:19:59 +0100

    using components

--- 03-course-project/recepie-app/src/app/app.module.ts
+++ 03-course-project/recepie-app/src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShippingEditComponent } from './shopping-list/shipping-edit/shipping-edit.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
     RecipesListComponent,
-    RecipeDetailComponent,
+
     RecipeItemComponent,
     ShoppingListComponent,
-    ShippingEditComponent,
+
+    RecipesDetailsComponent,
+     ShoppingEditComponent,
   ],
   imports: [BrowserModule],
   providers: [],
