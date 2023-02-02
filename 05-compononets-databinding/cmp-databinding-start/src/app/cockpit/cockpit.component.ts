fc5aedd1337deac56217046968edbf6d0183282f
Maykal Tenev <michaeltenev@gmail.com>
2023-02-02 16:44:36 +0100

    local references

--- 05-compononets-databinding/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ 05-compononets-databinding/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -21,16 +21,17 @@ export class CockpitComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
       serverContent: this.newServerContent,
     });
   }
