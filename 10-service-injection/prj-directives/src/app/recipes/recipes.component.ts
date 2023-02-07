2a60b7f9dba59843019f2eb758f40feeb2f4cb96
Maykal Tenev <michaeltenev@gmail.com>
2023-02-07 15:53:17 +0100

    project servivce injections

---
+++ 10-service-injection/prj-directives/src/app/recipes/recipes.component.ts
@@ -0,0 +1,18 @@
+import { RecipeService } from "./recepie.service";
+import { Component, OnInit } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+
+@Component({
+  selector: "app-recipes",
+  templateUrl: "./recipes.component.html",
+  styleUrls: ["./recipes.component.css"],
+  providers: [RecipeService],
+})
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
