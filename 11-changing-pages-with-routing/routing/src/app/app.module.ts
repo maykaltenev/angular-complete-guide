e79c86b89936b66746ab1b4d280369339da88bbb
Maykal Tenev <michaeltenev@gmail.com>
2023-04-03 23:03:41 +0200

    server components

--- 11-changing-pages-with-routing/routing/src/app/app.module.ts
+++ 11-changing-pages-with-routing/routing/src/app/app.module.ts
@@ -14,8 +14,9 @@ import { Routes, RouterModule } from "@angular/router";
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "users", component: UsersComponent },
-  { path: "users/:id", component: UserComponent },
+  { path: "users/:id/:name", component: UserComponent },
   { path: "servers", component: ServersComponent },
+  { path: "servers/:id", component: ServerComponent },
   { path: "servers/:id/edit", component: EditServerComponent },
 ];
 @NgModule({
